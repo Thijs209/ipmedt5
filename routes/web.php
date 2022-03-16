1267684c8acc1854492a0fbea8995cd8738ed8b6
NGHRD <s1127500@student.hsleiden.com>
2022-03-16 11:00:23 +0100

    Model, Controller en basic view voor aantal personen in kamer

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\RoomController::class, 'show']);
 
 Route::get('/Nils', function() {
     echo "Hallo Nils";
