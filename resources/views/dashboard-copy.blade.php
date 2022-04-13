ce45c5c2c087f1817f0896ef2d63377f05d49763
NGHRD <s1127500@student.hsleiden.com>
2022-04-13 17:49:36 +0200

    Realtime functie

--- resources/views/dashboard-copy.blade.php
+++ resources/views/dashboard-copy.blade.php
@@ -5,6 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>dashboard</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+    <script>
+        var pusher = new Pusher('45da88aa7f5d38d338c4', {
+        cluster: 'eu'
+        });
+
+        var channel = pusher.subscribe('my-channel');
+        channel.bind('dashboard-update', function() {
+            location.reload();
+        });
+    </script>
 </head>
 <body>
     
