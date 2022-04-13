89499481b9dee1efe7f6893d6921af35979f71a4
Thijs209 <s1128756@student.hsleiden.nl>
2022-04-13 22:50:05 +0200

    mqtt werkt

--- mqtt.py
+++ mqtt.py
@@ -1,69 +1,57 @@
-#
-# Copyright 2021 HiveMQ GmbH
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import time
+import sys
 import paho.mqtt.client as paho
-from paho import mqtt
 import json
 
-# setting callbacks for different events to see if it works, print the message etc.
-def on_connect(client, userdata, flags, rc, properties=None):
-    print("CONNACK received with code %s." % rc)
 
-# with this callback you can see if your publish was successful
-def on_publish(client, userdata, mid, properties=None):
-    print("mid: " + str(mid))
+def change_lights(aan, roomID):
+    # setting callbacks for different events to see if it works, print the message etc.
+    def on_connect(client, userdata, flags, rc, properties=None):
+        print("CONNACK received with code %s." % rc)
 
-# print which topic was subscribed to
-def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+    # with this callback you can see if your publish was successful
+    def on_publish(client, userdata, mid, properties=None):
+        print("mid: " + str(mid))
 
-# print message, useful for checking if it was successful
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    # print which topic was subscribed to
+    def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+        print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
-# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
-# userdata is user defined data of any type, updated by user_data_set()
-# client_id is the given name of the client
-client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
-# client = mqtt.Client(transport="websockets")
-client.on_connect = on_connect
+    # print message, useful for checking if it was successful
+    def on_message(client, userdata, msg):
+        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    
+    client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+    # client = mqtt.Client(transport="websockets")
+    client.on_connect = on_connect
 
-# enable TLS for secure connection
-#client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-#client.tls_insecure_set(True)
-# set username and password
-# client.username_pw_set("", "")
-# connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("localhost", 1883)
+    # enable TLS for secure connection
+    #client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    #client.tls_insecure_set(True)
+    # set username and password
+    # client.username_pw_set("", "")
+    # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+    client.connect("localhost", 1883)
 
-# setting callbacks, use separate functions like above for better visibility
-client.on_subscribe = on_subscribe
-client.on_message = on_message
-client.on_publish = on_publish
+    # setting callbacks, use separate functions like above for better visibility
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+    client.on_publish = on_publish
 
-# subscribe to all topics of encyclopedia by using the wildcard "#"
-client.subscribe("encyclopedia/#", qos=1)
-client.subscribe("domoticz/in", qos=1)
+    # subscribe to all topics of encyclopedia by using the wildcard "#"
+    client.subscribe("encyclopedia/#", qos=1)
+    client.subscribe("domoticz/in", qos=1)
 
-# a single publish, this can also be done in loops, etc.
-client.publish("encyclopedia/temperature", payload="cold", qos=1)
-client.publish("domoticz/in", json.dumps({'idx' : 1, 'nvalue' : 0, 'svalue' : '25.0' }), qos=1)
+    # a single publish, this can also be done in loops, etc.
+    temp = '0'
+    print(aan)
+    if aan == 'True':
+        temp = '100'
+    else:
+        temp = '1'
+        
+    client.publish("encyclopedia/temperature", payload="cold", qos=1)
+    client.publish("domoticz/in", json.dumps({'idx' : 1, 'nvalue' : 0, 'svalue' : temp}), qos=1)
+    sys.exit('sent')
 
-client.publish("{ 'idx' : 1, 'nvalue' : 0, 'svalue' : '0.0' }", "domoticz/in")
-
-# loop_forever for simplicity, here you need to stop the loop manually
-# you can also use loop_start and loop_stop
-client.loop_forever()
+if __name__ == '__main__':
+    change_lights(sys.argv[1], sys.argv[2])
--- python/main.py
+++ python/main.py
@@ -4,40 +4,33 @@ from nis import cat
 import serial
 import os
 
-import time
 import mysql.connector
 import paho.mqtt.client as paho
-from paho import mqtt
-import json
 
 mydb = mysql.connector.connect(
     host = "localhost",
     user="thijs",
-    passwd="123",
+    passwd="",
     database="light_control"
 )
-
-def change_lights():
-    if(mycursor.execute("SELECT people FROM rooms") <= 0):
-        print("lights out")
-    else:
-        print("lights on")
 
 def check_people(roomID):
     mycursor.execute("SELECT people FROM rooms WHERE id = '" + roomID + "'")
     people = mycursor.fetchone()
-    print(people)
-    if people == 0:
+    print(people[0])
+    if people[0] < 0:
+        mycursor.execute("UPDATE rooms SET people = 0 WHERE id = '" + roomID + "';")
+        mydb.commit()
+
+    if people[0] == 0:
         return False
     else:
         return True
 
 if __name__ == '__main__':
-    mycursor = mydb.cursor()
-    peoples = mycursor.fetchall()
+    mycursor = mydb.cursor(buffered = True)
 
-    port = serial.Serial("/dev/ttyUSB0", baudrate="115200", timeout=3.0)
-
+    port = serial.Serial("/dev/ttyUSB1", baudrate="115200", timeout=3.0)
 
     while True:
         rcv = port.readline().decode('utf-8').rstrip()
@@ -50,98 +43,37 @@ if __name__ == '__main__':
                 
             except:
                 print(roomID + 'bestaat al')
+            rcv = ""
 
         if "+1" in rcv:
-            print("detected2")
             roomID = rcv.split()[0]
             try:
-                mycursor.execute("UPDATE rooms SET people = people +1 WHERE id =" + roomID + ";")
+                mycursor.execute("UPDATE rooms SET people = people +1 WHERE id = '" + roomID + "';")
                 mydb.commit()
 
                 if check_people(roomID):
-                    change_lights(True, roomID)
+                    os.system("python mqtt.py True " + roomID)
                 else:
-                    change_lights(False, roomID)
-
-                # os.system("python mqtt+1.py")
-
+                    os.system("python mqtt.py False " + roomID)
             except:
-                print(roomID + "bestaat niet")
-            rcv = 0
+                print(roomID + " bestaat niet")
+
+            rcv = ""
                 
         if '-1' in rcv:
-            print("detected1")
             roomID = rcv.split()[0]
             try:
-                mycursor.execute("UPDATE rooms SET people = people -1 WHERE id =" + roomID + ";")
+                mycursor.execute("UPDATE rooms SET people = people -1 WHERE id = '" + roomID + "';")
                 mydb.commit()
 
                 if check_people(roomID):
-                    change_lights(True, roomID)
+                    os.system("python mqtt.py True " + roomID)
                 else:
-                    change_lights(False, roomID)
-
-
-                # os.system("/python/python mqtt.py")
+                    os.system("python mqtt.py False " + roomID)
             except:
                 print(roomID + "bestaat niet")
-            rcv = 0
-
-
+            rcv = ""
 
     mydb.close()
 
 
-def light_change(aan, roomID):
-    # setting callbacks for different events to see if it works, print the message etc.
-    def on_connect(client, userdata, flags, rc, properties=None):
-        print("CONNACK received with code %s." % rc)
-
-    # with this callback you can see if your publish was successful
-    def on_publish(client, userdata, mid, properties=None):
-        print("mid: " + str(mid))
-
-    # print which topic was subscribed to
-    def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-        print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-    # print message, useful for checking if it was successful
-    def on_message(client, userdata, msg):
-        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-    
-    client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
-    # client = mqtt.Client(transport="websockets")
-    client.on_connect = on_connect
-
-    # enable TLS for secure connection
-    #client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-    #client.tls_insecure_set(True)
-    # set username and password
-    # client.username_pw_set("", "")
-    # connect to HiveMQ Cloud on port 8883 (default for MQTT)
-    client.connect("localhost", 1883)
-
-    # setting callbacks, use separate functions like above for better visibility
-    client.on_subscribe = on_subscribe
-    client.on_message = on_message
-    client.on_publish = on_publish
-
-    # subscribe to all topics of encyclopedia by using the wildcard "#"
-    client.subscribe("encyclopedia/#", qos=1)
-    client.subscribe("domoticz/in", qos=1)
-
-    # a single publish, this can also be done in loops, etc.
-    client.publish("encyclopedia/temperature", payload="cold", qos=1)
-    temp = 0
-    if aan:
-        temp = 100
-    else:
-        temp = 1
-        
-    client.publish("domoticz/in", json.dumps({'idx' : 1, 'nvalue' : 0, 'svalue' : temp }), qos=1)
-
-    client.publish("{ 'idx' : 1, 'nvalue' : 0, 'svalue' : " + temp + "}", "domoticz/in")
-
-    # loop_forever for simplicity, here you need to stop the loop manually
-    # you can also use loop_start and loop_stop
-    client.loop_forever()
